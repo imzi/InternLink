9b2ccba26cb4fab39803e65eeca170d90543efb4
imzi <amimthath@gmail.com>
2024-09-09 08:58:04 +0530

    Successfully ran the init

--- src/main/java/com/internlink/InternLinkApplication.java
+++ src/main/java/com/internlink/InternLinkApplication.java
@@ -8,6 +8,7 @@ public class InternLinkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InternLinkApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
