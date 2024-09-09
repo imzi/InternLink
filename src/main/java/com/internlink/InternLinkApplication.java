1922fd890d5877c7ae461ee9cc245d6e133dbcb4
imzi <amimthath@gmail.com>
2024-09-09 08:36:48 +0530

    Project initialization

---
+++ src/main/java/com/internlink/InternLinkApplication.java
@@ -0,0 +1,13 @@
+package com.internlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internlink/InternLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
