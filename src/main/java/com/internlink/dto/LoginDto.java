e6ee3236fb8f88ec421f54f102aeb14c4af8f6fe
imzi <amimthath@gmail.com>
2024-12-03 22:10:39 +0530

    removed authentication

--- src/main/java/com/internlink/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.internlink.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginDto {
-    private String username;
-    private String password;
-}
--- src/main/java/com/internlink/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.internlink.service;
-
-import com.internlink.dto.LoginDto;
-import com.internlink.model.User;
-
-public interface UserService {
-    void save(User user);
-    void login(LoginDto loginUser);
-}
